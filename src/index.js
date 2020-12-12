2a05bada66cddba8c8594d8b201f627fd73915d6
John Bede <johnbede2000@gmail.com>
2020-12-12 20:39:49 +0000

    events

--- src/index.js
+++ src/index.js
@@ -60,12 +60,17 @@ function ChetAlbums() {
 }
 
 const Album = ({ img, title, year }) => {
+  const clickFunction = (title) => {
+    alert('You clicked ' + title);
+  };
   return (
     <article className="album">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h2>{year}</h2>
-      <button type="button">Click Here</button>
+      <button type="button" onClick={() => clickFunction(title)}>
+        Click Here
+      </button>
     </article>
   );
 };
