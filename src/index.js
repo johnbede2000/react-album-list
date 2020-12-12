9f087fa4b2ea7754b66f249d9592e99ecbdfcbd2
John Bede <johnbede2000@gmail.com>
2020-12-12 13:27:13 +0000

    iterate over an array

--- src/index.js
+++ src/index.js
@@ -4,38 +4,58 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css';
 
+const albums = [
+  {
+    title: 'Chet Baker Sings',
+    year: '1955',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/61wPRUL2vvL._AC_SL1200_.jpg',
+  },
+
+  {
+    title: 'It Could Happen To You',
+    year: '1958',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/715y1W38bxL._AC_SL1400_.jpg',
+  },
+
+  {
+    title: 'She Was Too Good To Me',
+    year: '1974',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/A169GHifGHL._AC_SL1500_.jpg',
+  },
+
+  {
+    title: 'Chet Baker In Tokyo',
+    year: '1987',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/411ADRBH6ZL._AC_.jpg',
+  },
+];
+
 function ChetAlbums() {
   return (
     <section>
       <p>Here are some of my favourite albums by Chet Baker:</p>
       <div className="list">
-        <Album />
-        <Album />
-        <Album />
-        <Album />
+        {albums.map((album) => {
+          return <Album album={album}></Album>;
+        })}
       </div>
     </section>
   );
 }
 
-const Album = () => {
+const Album = (props) => {
+  const { img, title, year } = props.album;
   return (
-    <article>
-      <Image />
-      <Title />
-      <Year />
+    <article className="album">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h2>{year}</h2>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/61wPRUL2vvL._AC_SL1200_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>Chet Baker Sings</h1>;
-const Year = () => <h2>1955</h2>;
 
 ReactDom.render(<ChetAlbums />, document.getElementById('root'));
